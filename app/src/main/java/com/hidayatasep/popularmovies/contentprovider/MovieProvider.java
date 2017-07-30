e75840d04624513511e0c683afcb5a3baa3e892b
asep Hidayat <hidayatasep2901@gmail.com>
2017-07-30 09:54:10 +0700

    content provider

--- app/src/main/java/com/hidayatasep/popularmovies/contentprovider/MovieProvider.java
+++ app/src/main/java/com/hidayatasep/popularmovies/contentprovider/MovieProvider.java
@@ -1,15 +1,19 @@
 package com.hidayatasep.popularmovies.contentprovider;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
 import java.util.HashMap;
 
@@ -19,7 +23,7 @@ import java.util.HashMap;
 
 public class MovieProvider extends ContentProvider {
 
-    public static final String PROVIDER_NAME = "com.hidayatasep.popularmovies.MovieProvider";
+    public static final String PROVIDER_NAME = "com.hidayatasep.popularmovies.movieprovider";
     public static final String URL_PROVIDER = "content://" + PROVIDER_NAME + "/movie";
     public static final Uri CONTENT_URI = Uri.parse(URL_PROVIDER);
 
@@ -99,29 +103,119 @@ public class MovieProvider extends ContentProvider {
 
     @Nullable
     @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
-        return null;
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection,
+                        @Nullable String selection, @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(MOVIE_TABLE_NAME);
+
+        switch (uriMatcher.match(uri)){
+            case MOVIE:
+                queryBuilder.setProjectionMap(MOVIE_PROJECTION_MAP);
+                break;
+            case MOVIE_ID:
+                queryBuilder.appendWhere(_ID + "=" + uri.getPathSegments().get(1));
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+
+        if(sortOrder == null || sortOrder.equals("")){
+            /**
+             * By default sort on student names
+             */
+            sortOrder = TITLE;
+        }
+
+        Cursor c = queryBuilder.query(db,	projection,	selection,
+                selectionArgs,null, null, sortOrder);
+        /**
+         * register to watch a content URI for changes
+         */
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+
     }
 
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+        switch (uriMatcher.match(uri)){
+            /**
+             * Get all student records
+             */
+            case MOVIE:
+                return "vnd.android.cursor.dir/vnd.com.hidayatasep.popularmovies.movieprovider.movie";
+            /**
+             * Get a particular student
+             */
+            case MOVIE_ID:
+                return "vnd.android.cursor.item/vnd.com.hidayatasep.popularmovies.movieprovider.movie";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+        //add a new movie record
+        long rowID = db.insert(MOVIE_TABLE_NAME,"",contentValues);
+
+        /**
+         * If record is added successfully
+         */
+        if(rowID > 0){
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI,rowID);
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+
+        throw new SQLException("Failed to add a record into " + uri);
+
     }
 
     @Override
-    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int count = 0;
+
+        switch (uriMatcher.match(uri)){
+            case MOVIE:
+                count = db.delete(MOVIE_TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case MOVIE_ID:
+                String id = uri.getPathSegments().get(1);
+                count = db.delete( MOVIE_TABLE_NAME, _ID +  " = " + id +
+                                (!TextUtils.isEmpty(selection) ? " AND (" + selection + ')' : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
     }
 
     @Override
-    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
-        return 0;
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int count = 0;
+        switch (uriMatcher.match(uri)) {
+            case MOVIE:
+                count = db.update(MOVIE_TABLE_NAME, contentValues, selection, selectionArgs);
+                break;
+
+            case MOVIE_ID:
+                count = db.update(MOVIE_TABLE_NAME, contentValues,
+                        _ID + " = " + uri.getPathSegments().get(1) +
+                                (!TextUtils.isEmpty(selection) ? " AND (" +selection + ')' : ""), selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI " + uri );
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
     }
 }
